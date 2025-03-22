e337e16bce36aae3d23b03f0257d3493534aef6d
R-DK <21f2001114@ds.study.iitm.ac.in>
2025-03-22 21:21:14 +0530

    feat: Added basic application routes

---
+++ BACKEND/application/routes.py
@@ -0,0 +1,32 @@
+from flask import current_app as app  # use the app object created in the app.py file
+from application.database import db
+from flask import jsonify, request
+from flask_security import auth_required, roles_required, hash_password
+from datetime import datetime
+
+
+@app.route('/admin')
+@auth_required('token')  # this decorator ensures that the user is authenticated
+@roles_required('admin')  # this decorator ensures that the user has the 'admin' role (RBAC)
+def admin_home():
+    return jsonify({"message": "Welcome Admin!"})
+
+
+@app.route('/register', methods=["POST"])
+def register():
+    data = dict(request.get_json())
+    if not data or not {"email", "password", "firstname", "qualification"}.issubset(data.keys()):
+        return jsonify({"message": "Invalid Data"}), 400
+    if app.security.datastore.find_user(email=data["email"]):
+        return jsonify({"message": "User already exists"}), 400
+    app.security.datastore.create_user(
+        email=data["email"],
+        password=hash_password(data["password"]),
+        firstname=data.get("firstname"),
+        active=True,
+        roles=["user"],
+        created_at=datetime.now()
+    )
+
+    db.session.commit()
+    return jsonify({"message": "User Created"}), 201
